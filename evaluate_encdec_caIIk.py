df3197d7bf058cba3cd2bc15b14dd6cb5c888911
Harsh Mathur <harshmathur.1990@gmail.com>
2021-05-04 09:54:36 +0530

    changes generic node

--- evaluate_encdec_caIIk.py
+++ evaluate_encdec_caIIk.py
@@ -109,9 +109,9 @@ emission_nodes = [
 ]
 
 generic_nodes = [
-    np.array([65, 112, 144]),
-    np.array([50,65, 112, 144]),
-    np.array([65, 112, 144])
+    np.array([81, 106, 116, 123, 131, 140]),
+    np.array([81, 106, 116, 123, 131, 140]),
+    np.array([81, 106, 116, 123, 131, 140])
 ]
 
 def get_nodes(nodename='emission'):
